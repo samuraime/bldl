dd5dcbbd844f3d10bfe2187b7011945d1556eb46
Minhui Zhao <samurai7@foxmail.com>
2023-03-19 23:21:07 +1100

    feat: display codec in progress bar

--- src/downloadTracks.js
+++ src/downloadTracks.js
@@ -6,22 +6,28 @@ import { downloadTrack } from './utils.js';
 function downloadTracks(context, { metadata, tracks }) {
   const multiBar = new cliProgress.MultiBar(
     {
-      clearOnComplete: true,
+      clearOnComplete: false,
       hideCursor: true,
-      format: ' {bar} | {title} | {mimeType} | {transferred}/{size}',
+      format: ' {bar} | {title} | {mimeType} | {codec} | {transferred}/{size}',
     },
     cliProgress.Presets.shades_grey,
   );
 
   const downloadedTracks = tracks.map((track) => {
-    const bar = multiBar.create(1, 0, {title: metadata.title, mimeType: track.mimeType, transferred: 0, size: 'Unknown'});
+    const bar = multiBar.create(1, 0, {
+      title: metadata.title,
+      mimeType: track.mimeType,
+      codec: track.codec,
+      transferred: 0,
+      size: 'Unknown',
+    });
 
     return downloadTrack({
       track,
       credential: context.credential,
       saveToDirectory: path.resolve(context.tmpDir, metadata.title),
       onProgress: ({ percent, transferred, total }) => {
-        bar.update(percent, { title: metadata.title, mimeType: track.mimeType, transferred, size: total });
+        bar.update(percent, { transferred, size: total });
       },
     })
       .then(((downloadedTrackFile) => ({
