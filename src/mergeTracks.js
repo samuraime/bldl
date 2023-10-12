8a962cb3fb896bd11f70ca2fab4a3ee8dec008ab
Minhui Zhao <samurai7@foxmail.com>
2023-10-12 22:47:21 +1100

    fix: apple compatible hevc

--- src/mergeTracks.js
+++ src/mergeTracks.js
@@ -18,6 +18,10 @@ function escapeSpecialCharacter(string) {
   return string.replace(specialChar, '-');
 }
 
+function doubleQuote(string) {
+  return `"${string.replace(/"/g, '\\"')}"`;
+}
+
 async function getOutputPath(output, defaultFileName) {
   const escapedDefaultFileName = escapeSpecialCharacter(defaultFileName);
 
@@ -39,10 +43,23 @@ async function getOutputPath(output, defaultFileName) {
 }
 
 async function mergeTracks(context, { metadata, tracks }) {
-  const output = await getOutputPath(context.output, `${metadata.title}.mp4`);
+  const output = await getOutputPath(
+    context.output,
+    `${metadata.title}.mp4`
+  ).then(doubleQuote);
 
-  const inputs = tracks.map((track) => `-i "${track.path}"`).join(' ');
-  const ffmpegCommand = `ffmpeg ${inputs} -c copy "${output}" -y`;
+  const inputs = tracks
+    .map((track) => track.path)
+    .map(doubleQuote)
+    .map((input) => `-i ${input}`)
+    .join(' ');
+
+  // `-tag:v hvc1` for Apple HEVC compatibility https://trac.ffmpeg.org/wiki/Encode/H.265#FinalCutandApplestuffcompatibility
+  const outputs = tracks.some(({ codec }) => codec.includes('hev1'))
+    ? `-c:v copy -tag:v hvc1 -c:a copy ${output}`
+    : `-c copy ${output}`;
+
+  const ffmpegCommand = `ffmpeg ${inputs} ${outputs} -y`;
 
   await promisifiedExec(ffmpegCommand).catch((error) => {
     context.keepTmpTracks = true; // Keep tmp tracks in case we'd like to merge them manually
